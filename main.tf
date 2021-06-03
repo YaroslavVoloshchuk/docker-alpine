e158e80ae771895630dc417297207d27c4a43185
Yaroslav Voloshchuk <bamiks@gmail.com>
2021-06-03 12:09:57 +0300

    change main.tf

--- main.tf
+++ main.tf
@@ -13,30 +13,26 @@ provider "docker" {
 }
 
 # Pulls the image
-resource "docker_image" "jenkins" {
-  name = "jenkins/jenkins:2.295-alpine"
+resource "docker_image" "example-alpine" {
+  name = "bamik/docker-alpine:latest"
   keep_locally = true
 }
 
 # Create a container
-resource "docker_container" "jenkins" {
-  image = docker_image.jenkins.latest
-  name  = "jenkins-serva4ok"
+resource "docker_container" "example-alpine" {
+  image = docker_image.example-alpine.latest
+  name  = "php-site"
   restart = "always"
   privileged = "true"
   ports {
     internal = 8080
-    external = 8889
+    external = 8890
   }
   volumes {
-    container_path = "/var/jenkins_home"
-    host_path      = "/var/jenkins_data"
+    container_path = "/var/www/html"
+    host_path      = "/var/www/alpine-site"
     
   }
- volumes {
-    container_path = "/var/run/docker.sock"
-    host_path      = "/var/run/docker.sock"
-
-  }
+ 
 
 }
